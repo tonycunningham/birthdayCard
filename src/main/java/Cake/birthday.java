6e897f5bcf684c9e9bbf98719bb16291c64b63ad
Tony Cunningham <tony.cunningham83@gmail.com>
2023-04-28 11:34:03 -0400

    Pushing work

---
+++ src/main/java/Cake/birthday.java
@@ -0,0 +1,156 @@
+package Cake;
+
+import java.util.Scanner;
+
+public class birthday {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        birthday app = new birthday();
+        app.run();
+    }
+
+    public void itsArlosBirthday(){
+        System.out.println();
+        System.out.println("                    0   0");
+        System.out.println("                    |   |");
+        System.out.println("                ____|___|____");
+        System.out.println("             0  |~ ~ ~ ~ ~ ~|   0");
+        System.out.println("             |  |           |   |");
+        System.out.println("          ___|__|___________|___|__");
+        System.out.println("          |/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/|");
+        System.out.println("      0   |   H a p p y  11th     |   0");
+        System.out.println("      |   |/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/|   |");
+        System.out.println("     _|___|_______________________|___|__");
+        System.out.println("    |/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/|");
+        System.out.println("    |          B i r t h d a y          |");
+        System.out.println("    |             A R L O !!            |");
+        System.out.println("    | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |");
+        System.out.println("    |___________________________________|");
+    }
+
+    public void blowOutCandles(){
+        System.out.println("");
+        System.out.println("                   `     `  ");
+        System.out.println("                    `   `  ");
+        System.out.println("                    |   |");
+        System.out.println("            `   ____|___|____    `");
+        System.out.println("             `  |~ ~ ~ ~ ~ ~|   ` ");
+        System.out.println("             |  |           |   |");
+        System.out.println("          ___|__|___________|___|__");
+        System.out.println("     `    |/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/|     `");
+        System.out.println("      `   |   H a p p y  11th     |    `");
+        System.out.println("      |   |/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/|   |");
+        System.out.println("     _|___|_______________________|___|__");
+        System.out.println("    |/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/|");
+        System.out.println("    |          B i r t h d a y          |");
+        System.out.println("    |             A R L O !!            |");
+        System.out.println("    | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |");
+        System.out.println("    |___________________________________|");
+    }
+
+    public void balloons() {
+        System.out.println("");
+        System.out.println("        ,,,,,,,,,,,,,");
+        System.out.println("    .;;;;;;;;;;;;;;;;;;;,.");
+        System.out.println("  .;;;;;;;;;;;;;;;;;;;;;;;;,");
+        System.out.println(".;;;;;;;;;;;;;;;;;;;;;;;;;;;;.");
+        System.out.println(";;;;;@;;;;;;;;;;;;;;;;;;;;;;;;' .............");
+        System.out.println(";;;;@@;;;;;;;;;;;;;;;;;;;;;;;;'.................");
+        System.out.println(";;;;@@;;;;;;;;;;;;;;;;;;;;;;;;'...................");
+        System.out.println("`;;;;@;;;;;;;;;;;;;;;@;;;;;;;'.....................");
+        System.out.println(" `;;;;;;;;;;;;;;;;;;;@@;;;;;'..................;....");
+        System.out.println("  `;;;;;;;;;;;;;;;;@@;;;;'....................;;...");
+        System.out.println("    `;;;;;;;;;;;;;@;;;;'...;.................;;....");
+        System.out.println("      `;;;;;;;;;;;;'   ...;;...............;.....");
+        System.out.println("         `;;;;;;'        ...;;..................");
+        System.out.println("            ;;              ..;...............");
+        System.out.println("            `                  ............");
+        System.out.println("           `                      ......");
+        System.out.println("          `                         ..");
+        System.out.println("         `                           '");
+        System.out.println("        `                           '");
+        System.out.println("       `                           '");
+        System.out.println("      `                           `");
+        System.out.println("      `                           `,");
+        System.out.println("      `");
+        System.out.println("       `");
+        System.out.println("         `.");
+        System.out.println("            DONT LET THEM FLY AWAY!!!!");
+
+    }
+    public void openPresent(){
+        System.out.println("  ⠀⠀⠀⠀⠀⠀⣸⠃⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣧⡀⠀⠉⠉⠈⠀⢀⣼⣿⣿⣿⣿⣿⠿⠟⠛⠛⠋⠛⠓⢦⣄⠀⠀⠀⢹⣷⣿⣿⣿⣷⣾⣿⣿⣿⡿⣀⣠⣤⣤⣤⣤");
+        System.out.println("⠀⠀⠀⠀⠀⠀⠀⢠⢿⠃⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣿⣿⠿⠛⠉⠁⠀⠀⠀⠈⠁⠀⠉⠉⠙⢿⣄⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠀⠀⠀⣸⡇⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠉⠀⠀⠀⠀⠀⣀⣠⣴⣶⣾⣿⣿⣿⣿⣿⣿⡆⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠀⠀⠀⣿⡻⢠⣿⣟⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢋⢁⣠⠖⠀⠀⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠀⠀⠀⣽⣧⣿⣿⣿⣷⣍⠉⠻⣯⢿⣿⣿⣿⡿⢛⣡⣶⡿⠛⢁⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠀⠀⠀⢷⢻⣿⣿⣿⣿⣿⡆⡈⠁⢿⣿⠿⠉⡤⠚⠁⢀⣤⣶⣿⣿⣿⣿⣿⣿HAPPY⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣶⠟⠁⠀⠀⢀⣠⠖⠋⠀⠈⢻⣿⣿⣿⣿BIRTHDAY⣿⣿⣿⣿⣿⣿⣿⣿⡿⢡⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠀⠀⠀⢸⢿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⢀⣴⡏⠁⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿ARLO⣿⣿⣿⣿⣿⣿⣿⣿⡿⢋⣴⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⢟⡁⠀⣠⣾⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢟⣫⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣟⣵⠟⣠⣾⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⠿⢛⡭⠞⠛⣏⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⢻⣿⠟⢡⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠹⣿⡿⢟⢛⣩⡔⠚⠁⠀⠀⠀⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠀⠀⣸⣿⣿⣵⠿⢃⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⣀⣠⠴⠖⠋⢉⣼⠀⠀⠀⠀⢀⣼⡇⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⢀⣾⣿⠿⡾⠋⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⢛⣛⣋⡵⠶⣶⣞⠋⠁⠀⠀⠀⠀⠀⣿⣶⣶⣶⣾⡿⠟⠁⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⣀⣴⣿⣿⠃⣀⣾⣟⣛⣛⣛⣛⣛⣻⣿⠽⢥⣶⠞⠒⠛⠉⠉⠀⠀⠀⠀⠻⢷⠂⠀⠀⠀⣠⠞⢡⡀⣿⠉⠉⠁⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣶⡿⢟⠿⠉⣿⡶⠿⡟⢿⣠⡿⠋⣩⠉⣽⠏⠀⢸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡏⠀⠀⠀⣸⠃⠀⠀⠳⡈⢓⣦⣤⣀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣤⠴⡷⣾⣟⣿⠁⠀⣷⣮⡟⠀⣴⡏⣼⡟⠀⠀⠀⠻⣧⣄⠀⠀⠀⠀⢀⣤⣾⢟⡾⠁⠀⣼⠃⠀⠀⠀⣠⣷⡋⢠⡶⠟⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠤⣤⣀⣀⠈⠙⣆⠀⢸⣿⠀⣸⣿⢻⣿⠁⠀⠀⠀⠀⠉⠛⣿⠿⠾⠿⠛⠋⣠⠞⠀⢀⡾⠁⠀⢀⣴⢞⡿⣟⣅⣀⠘⠲⢦⣀⡀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⡀⠹⣄⣨⠟⣦⣼⡆⢰⡇⢰⡿⠟⣸⠋⠄⠀⠀⠀⠀⠀⠀⠈⠉⠁⢠⣴⣛⣁⡤⠞⠉⠀⠀⠀⠘⠛⠋⠉⠀⠀⠉⠳⠦⠄⠀⣉⣥⠞⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⡈⢻⠀⢰⠞⠃⠉⠹⣼⠃⣾⠃⢰⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠖⢻⡟⠁⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⡓⠘⣧⡘⢦⠾⢀⣲⡿⠀⠀⢠⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠟⢿⡄⣠⠞⣿⡀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠙⢦⣈⠙⠒⠒⠶⠆⡇⠀⢠⣿⠻⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢾⣭⠏⠀⠀⠹⣷⡆⢻⡇⣸⠋⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⣀⣼⠛⠷⣶⣦⣴⠇⢠⣿⣇⠀⠻⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠟⠉⠁⠀⢀⣀⡤⠞⡉⣠⠞⢀⡏⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣿⡿⠁⣠⡾⢻⡟⢹⠀⣾⣿⣿⢧⡀⠈⢷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠛⡹⢟⡿⢿⠛⢿⣁⣱⠴⠛⠁⠀⣸⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣛⣴⡾⠋⢀⣿⠁⣾⣿⣿⢻⣿⠈⠳⡄⠈⢷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠔⠛⠛⠓⠓⠚⠒⠚⠋⠉⠁⠀⠀⠀⠀⢠⡇⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠉⢹⣷⣤⣬⣿⢸⡿⣧⢸⣿⣿⡆⠀⠙⣦⡈⣯⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠁⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠈⢻⡆⠀⣿⣿⠃⢹⣿⣿⡇⣧⠀⠀⠈⠻⣼⣇⠈⠳⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡻⠶⢶⣦⡄⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠈⣷⡀⣿⣿⡆⠈⢻⣿⣿⣍⠀⠀⠀⠀⠈⠻⣆⠀⠀⠙⣿⡶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⡋⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠸⣇⠁⠸⣷⡄⣸⣿⣿⣿⣿⣦⣄⣀⠀⠀⠈⠣⠀⠀⠈⠻⣦⣍⠛⠳⠶⢤⣄⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣤⣴⣾⣿⣿⣿⣿⣿⣿⣄⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⢿⠀⠀⢹⣄⣿⣿⣿⣿⣿⣿⣿⣿⣟⣶⣦⣤⣀⣀⡀⠀⠈⠻⢷⣄⠀⠀⠀⣼⠛⣏⢭⠉⠙⢯⡿⠉⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠘⠇⠀⠀⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⣯⣿⣿⣗⣒⡶⠿⣷⣦⣤⣿⣤⣿⣬⣤⣤⣯⣤⣤⣤⣾⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠀⢤⠀⠀⢸⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣯⣽⣭⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠀⠀⡀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠀⠀⠀⠀⠀⠀⠙⠂⣼⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢃⠠⢸⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+    }
+
+    private void run() {
+        String menuSelection = "";
+        while (menuSelection != "hello") {
+            printMainMenu();
+            menuSelection = promptForMenuSelection();
+            if (menuSelection.equalsIgnoreCase("11")) {
+                itsArlosBirthday();
+            } else if (menuSelection.equalsIgnoreCase("balloons")) {
+                balloons();
+            } else if (menuSelection.equalsIgnoreCase("blow")) {
+                blowOutCandles();
+            } else if (menuSelection.equalsIgnoreCase("present")) {
+                openPresent();
+            } else if (menuSelection != "exit") {
+                System.out.println("Invalid Selection");
+            }
+        }
+    }
+    public void printMainMenu() {
+        System.out.println();
+        System.out.println("----Birthday Menu----");
+        System.out.println("Type how old you're turning here for a Birthday Cake");
+        System.out.println("Type Balloons if you want balloons for your birthday");
+        System.out.println("Type blow and make a wish to blow out your candles");
+        System.out.println("Type present to open your birthday present!");
+        System.out.println("Type Exit to exit program");
+        System.out.println();
+    }
+    public String promptForMenuSelection() {
+        String menuSelection;
+        System.out.print("Please choose an option: ");
+        try {
+            menuSelection = scanner.nextLine();
+        } catch (NumberFormatException e) {
+            menuSelection = "";
+        }
+        return menuSelection;
+    }
+}
---
+++ src/main/java/Cake/menu.java
@@ -0,0 +1,6 @@
+package Cake;
+
+public class menu {
+
+
+}
